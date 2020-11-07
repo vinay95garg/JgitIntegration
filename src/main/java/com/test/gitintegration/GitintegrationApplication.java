3e3da0fc34fb96b362c5635ca641271dbc5d9221
Garg <vigarg@ciena.com>
2020-11-07 11:18:18 +0530

    added cong

--- src/main/java/com/test/gitintegration/GitintegrationApplication.java
+++ src/main/java/com/test/gitintegration/GitintegrationApplication.java
@@ -1,13 +1,21 @@
 package com.test.gitintegration;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+
+import static org.eclipse.jgit.api.Git.*;
 
 @SpringBootApplication
 public class GitintegrationApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws GitAPIException {
         SpringApplication.run(GitintegrationApplication.class, args);
+        
     }
-
 }
