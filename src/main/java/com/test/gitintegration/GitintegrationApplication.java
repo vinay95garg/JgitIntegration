38547708a226824a941a3f49731fc5c32bce8f0e
Garg <vigarg@ciena.com>
2020-11-07 10:50:01 +0530

    inital changes

---
+++ src/main/java/com/test/gitintegration/GitintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.test.gitintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitintegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/gitintegration/GitintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gitintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitintegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
